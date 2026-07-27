b1b6cbd8385365859f33b349c670d159f6b8b970
bjarki <bjoernjanson.de@gmail.com>
2026-07-27 14:01:21 +0200

    Create __init__.py

---
+++ src/trainers/__init__.py
@@ -0,0 +1,12 @@
+from .base_trainer import BaseTrainer
+from .ancestor_trainer import AncestorTrainer
+from .curriculum import Curriculum
+from .evaluation_loop import EvaluationLoop
+
+
+__all__ = [
+    "BaseTrainer",
+    "AncestorTrainer",
+    "Curriculum",
+    "EvaluationLoop",
+]
