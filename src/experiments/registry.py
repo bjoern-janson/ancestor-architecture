469569887ff02ebaeb81bdc8f1c050a0cc7e6116
bjarki <bjoernjanson.de@gmail.com>
2026-07-27 14:29:25 +0200

    Create registry.py

---
+++ src/experiments/registry.py
@@ -0,0 +1,5 @@
+EXPERIMENT_REGISTRY = {
+    "agency_transfer": AgencyTransferExperiment,
+    "permeability_transition": PermeabilityTransitionExperiment,
+    "baseline_comparison": BaselineComparisonExperiment,
+}
