24d59b670534d1cd75ba06abd39d4fa06c5acf47
bjarki <bjoernjanson.de@gmail.com>
2026-07-27 14:03:50 +0200

    Create __init__.py

---
+++ src/visualization/__init__.py
@@ -0,0 +1,12 @@
+from .plots import create_plot
+from .phase_transition import plot_permeability_transition
+from .agency_curves import plot_agency_transfer
+from .ecosystem_landscape import plot_ecosystem_space
+
+
+__all__ = [
+    "create_plot",
+    "plot_permeability_transition",
+    "plot_agency_transfer",
+    "plot_ecosystem_space",
+]
